b7db1802464867ca956549c6c3eff424cbe97b3f
Omar Dachia <omardachia@gmail.com>
2024-03-31 14:21:14 +0100

    comment added for student

--- q1.cpp
+++ q1.cpp
@@ -1,8 +1,12 @@
-#include <iostream>
+//We import all the resources we need. in this, we only need iostream for user input/output
+#include <iostream> //is a header file library that lets us work with input and output objects,
+// such as cout (used in line 5). Header files add functionality to C++ programs.
 
-using namespace std;
+using namespace std; //means that we can use names for objects and variables from the standard library.
 
-int main() {
-  cout << "Hello World!";
-  return 0;
+int main() { //This is called a function. Any code inside its curly brackets {} will be executed.
+  cout << "Hello World!"; //(pronounced "see-out") is an object used together with the insertion operator (<<) to output/print text. In our example, it will output "Hello World!"
+  //Note: Every C++ statement ends with a semicolon ;
+
+  return 0; //ends the main function.
 }
